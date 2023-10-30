e260cdb9b94325d1ec929f81ec4b73617ba95da7
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-30 21:29:27 +0100

    *

--- test/app/api/algoliaDelete/route.js
+++ test/app/api/algoliaDelete/route.js
@@ -1,21 +1,21 @@
-// import algoliasearch from 'algoliasearch'
-// import { revalidatePath } from 'next/cache'
+import algoliasearch from 'algoliasearch'
+import { revalidatePath } from 'next/cache'
 
-// export async function POST(request) {
-// 	const client = algoliasearch('CL6X1N5OU8', '27368288330117fe0ba275850c47d3a6')
-// 	const index = client.initIndex('test_members')
-// 	try {
-// 		const { slug } = await request.json()
-// 		index.deleteObject(slug).then(() => {
-// 			// done
-// 		})
+export async function POST(request) {
+	const client = algoliasearch('CL6X1N5OU8', '27368288330117fe0ba275850c47d3a6')
+	const index = client.initIndex('test_members')
+	try {
+		const { slug } = await request.json()
+		index.deleteObject(slug).then(() => {
+			// done
+		})
 
-// 		revalidatePath(`/medlem`)
+		revalidatePath(`/medlem`)
 
-// 		return new Response(console.log(`path is revaldated. ${slug} was removed`))
-// 	} catch (err) {
-// 		// If there was an error, Next.js will continue
-// 		// to show the last successfully generated page
-// 		return new Response(console.log(err))
-// 	}
-// }
+		return new Response(console.log(`path is revaldated. ${slug} was removed`))
+	} catch (err) {
+		// If there was an error, Next.js will continue
+		// to show the last successfully generated page
+		return new Response(console.log(err))
+	}
+}
--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -1,37 +1,37 @@
-// import { Client } from '@/lib/client'
-// import groq from 'groq'
-// import { revalidatePath } from 'next/cache'
-// import algoliasearch from 'algoliasearch'
-// export async function POST(req) {
-// 	const client = algoliasearch('CL6X1N5OU8', '27368288330117fe0ba275850c47d3a6')
-// 	const index = client.initIndex('test_members')
-// 	try {
-// 		const cmsData = await Client.fetch(groq`*[_type == "medlem"]{
-// 			name, 
-// 			_id,
-// 			certifications[]->{name},
-// 			connections[]->{name},
-// 			tag[]->{Name},
-// 			contactPerson,
-// 			"slug":slug.current,
-// 		}`)
-// 		cmsData.map((item) => {
-// 			const obj = {
-// 				objectID: item._id,
-// 				name: item.name,
-// 				tags: item.tag,
-// 				certifications: item.certifications,
-// 				connections: item.connections,
-// 				slug: item.slug,
-// 			}
-// 			return index.saveObjects(obj)
-// 		})
-// 		const slug = await req.json()
-// 		console.log(slug.slug)
-// 		revalidatePath(`/medlem`)
-// 		revalidatePath(`/medlem/${slug.slug}`)
-// 		return new Response(console.log(' slug that got updated', slug.slug))
-// 	} catch (err) {
-// 		return new Response(console.log(err))
-// 	}
-// }
+import { Client } from '@/lib/client'
+import groq from 'groq'
+import { revalidatePath } from 'next/cache'
+import algoliasearch from 'algoliasearch'
+export async function POST(req) {
+	const client = algoliasearch('CL6X1N5OU8', '27368288330117fe0ba275850c47d3a6')
+	const index = client.initIndex('test_members')
+	try {
+		const cmsData = await Client.fetch(groq`*[_type == "medlem"]{
+			name, 
+			_id,
+			certifications[]->{name},
+			connections[]->{name},
+			tag[]->{Name},
+			contactPerson,
+			"slug":slug.current,
+		}`)
+		cmsData.map((item) => {
+			const obj = {
+				objectID: item._id,
+				name: item.name,
+				tags: item.tag,
+				certifications: item.certifications,
+				connections: item.connections,
+				slug: item.slug,
+			}
+			return index.saveObjects(obj)
+		})
+		const slug = await req.json()
+		console.log(slug.slug)
+		revalidatePath(`/medlem`)
+		revalidatePath(`/medlem/${slug.slug}`)
+		return new Response(console.log(' slug that got updated', slug.slug))
+	} catch (err) {
+		return new Response(console.log(err))
+	}
+}
