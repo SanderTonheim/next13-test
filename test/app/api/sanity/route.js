91691780c892a068a59a6a6a62c52fc499a1fac7
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-30 22:08:34 +0100

    init index

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -3,8 +3,8 @@ import groq from 'groq'
 import { revalidatePath } from 'next/cache'
 import algoliasearch from 'algoliasearch'
 export async function POST(req) {
-	// const client = algoliasearch('CL6X1N5OU8', '27368288330117fe0ba275850c47d3a6')
-	// const index = client.initIndex('members')
+	const client = algoliasearch('CL6X1N5OU8', '27368288330117fe0ba275850c47d3a6')
+	const index = client.initIndex('test')
 	try {
 		const cmsData = await Client.fetch(groq`*[_type == "medlem"]{
 			name, 
@@ -15,17 +15,17 @@ export async function POST(req) {
 			contactPerson,
 			"slug":slug.current,
 		}`)
-		// cmsData.map((item) => {
-		// 	const obj = {
-		// 		objectID: item._id,
-		// 		name: item.name,
-		// 		tags: item.tag,
-		// 		certifications: item.certifications,
-		// 		connections: item.connections,
-		// 		slug: item.slug,
-		// 	}
-		// 	return index.saveObjects(obj)
-		// })
+		cmsData.map((item) => {
+			const obj = {
+				objectID: item._id,
+				name: item.name,
+				tags: item.tag,
+				certifications: item.certifications,
+				connections: item.connections,
+				slug: item.slug,
+			}
+			return index.saveObjects(obj)
+		})
 		const slug = await req.json()
 		console.log(slug.slug)
 		// revalidatePath(`/medlem`)
