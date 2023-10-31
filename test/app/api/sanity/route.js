6f2c73d913c1532f07cb31c03b716b8046045ec9
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-31 14:16:20 +0100

    *

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -2,7 +2,7 @@ import { Client } from '@/lib/client'
 import groq from 'groq'
 import { revalidatePath } from 'next/cache'
 import algoliasearch from 'algoliasearch'
-import { log } from 'console'
+
 export async function POST(req) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
@@ -17,15 +17,16 @@ export async function POST(req) {
 			contactPerson,
 			"slug":slug.current,
 		}`)
-		const filterData = cmsData.map((item) => {
-			return console.log(item.name)
-			// objectID: item._id,
-			// name: item.name,
-			// tags: item.tag,
-			// certifications: item.certifications,
-			// connections: item.connections,
-			// slug: item.slug,
-		})
+		console.log(cmsData)
+		// const filterData = cmsData.map((item) => {
+		// return console.log(item.name)
+		// objectID: item._id,
+		// name: item.name,
+		// tags: item.tag,
+		// certifications: item.certifications,
+		// connections: item.connections,
+		// slug: item.slug,
+		// })
 		// return index.saveObjects(obj)
 		// })
 		// console.log(slug.slug)
