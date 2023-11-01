4a9ca98541a18018594dbd856e8de490ade30dcc
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-01 15:18:21 +0100

    *

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -6,28 +6,26 @@ import algoliasearch from 'algoliasearch'
 export async function POST(req) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
-	try {
-		const body = await req.json()
-		// const cmsData = await Client.fetch(groq`*[_type == "medlem" && slug.current == '${slug}'][0]{
-		// 	name,
-		// 	_id,
-		// 	certifications[]->{name},
-		// 	connections[]->{name},
-		// 	tag[]->{Name},
-		// 	contactPerson,
-		// 	"slug":slug.current,
-		// }`)
-		const obj = {
-			objectID: body._id,
-			name: body.name,
-			slug: body.slug,
-		}
-		index.saveObject(obj)
-		// console.log(slug.slug)
-		// revalidatePath(`/medlem`)
-		// revalidatePath(`/medlem/${slug.slug}`)
-		return new Response(console.log(obj))
-	} catch (err) {
-		return new Response(console.log(err))
+	const body = await req.json()
+	console.log(body)
+
+	// const cmsData = await Client.fetch(groq`*[_type == "medlem" && slug.current == '${slug}'][0]{
+	// 	name,
+	// 	_id,
+	// 	certifications[]->{name},
+	// 	connections[]->{name},
+	// 	tag[]->{Name},
+	// 	contactPerson,
+	// 	"slug":slug.current,
+	// }`)
+	const obj = {
+		objectID: body._id,
+		name: body.name,
+		slug: body.slug,
 	}
+	index.saveObject(obj)
+	// console.log(slug.slug)
+	// revalidatePath(`/medlem`)
+	// revalidatePath(`/medlem/${slug.slug}`)
+	// return new Response(console.log(obj))
 }
