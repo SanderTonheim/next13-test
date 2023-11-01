642823a7c4a0d62de9a1f0c0b6d7636b844b4aba
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-01 15:26:18 +0100

    *

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -7,17 +7,19 @@ export async function POST(req) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
 	const body = await req.json()
-	console.log(body)
 
-	// const cmsData = await Client.fetch(groq`*[_type == "medlem" && slug.current == '${slug}'][0]{
-	// 	name,
-	// 	_id,
-	// 	certifications[]->{name},
-	// 	connections[]->{name},
-	// 	tag[]->{Name},
-	// 	contactPerson,
-	// 	"slug":slug.current,
-	// }`)
+	const cmsData = await Client.fetch(groq`*[_type == "medlem" && slug.current == '${slug}'][0]{
+		name,
+		_id,
+		certifications[]->{name},
+		connections[]->{name},
+		tag[]->{Name},
+		contactPerson,
+		"slug":slug.current,
+	}`)
+
+	console.log({ body: body, type: typeof cmsData, data: cmsData })
+
 	const obj = {
 		objectID: body._id,
 		name: body.name,
