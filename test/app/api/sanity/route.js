1d40abead55c189fa191a0455abba332c37c43a8
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-06 16:55:38 +0100

    /

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -6,7 +6,7 @@ export async function POST(req) {
 	const index = client.initIndex('Members')
 	const body = await req.json()
 
-	await index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, slug: body.slug })
+	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, certifications: body.certifications, slug: body.slug })
 	revalidatePath(`/medlem`)
 	// revalidatePath(`/medlem/${body.slug}`)
 	return new Response(console.log(body))
