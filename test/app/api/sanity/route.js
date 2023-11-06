f65edcecdf002e9ee9f925930b9016f7649f85b9
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-06 15:56:53 +0100

    /

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -7,11 +7,13 @@ export async function POST(req) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
 	const body = await req.json()
-	const tags = [...body.tag] || null
+	const tags = body.tag
+	console.log('tags', tags)
+	console.log(tags.length)
 
 	// index.saveObject({ objectID: body._id, name: body.name, slug: body.slug })
 	// revalidatePath(`/medlem`)
 	// revalidatePath(`/medlem/${body.slug}`)
 	// return new Response(console.log(obj))
-	return new Response(console.log('ok', tags))
+	return new Response(console.log('ok'))
 }
