14852c362049a92c3e138adf62acd76959ee007f
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-06 14:09:49 +0100

    set up adding members webhook

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -17,7 +17,7 @@ export async function POST(req) {
 		certifications: [body.certifications],
 		slug: body.slug,
 	}
-	index.partialUpdateObject(obj)
+	index.saveObject(obj)
 	// revalidatePath(`/medlem`)
 	revalidatePath(`/medlem/${body.slug}`)
 	// return new Response(console.log(obj))
