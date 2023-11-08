88a9018716e40b178c1b5b244e544c86d2bc09af
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-08 10:01:10 +0100

    added func for updating record

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -11,3 +11,14 @@ export async function POST(req) {
 	// revalidatePath(`/medlem/${body.slug}`)
 	return new Response(console.log(body))
 }
+
+export async function PUT(req) {
+	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
+	const index = client.initIndex('Members')
+	const body = await req.json()
+
+	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, certifications: body.certifications, slug: body.slug })
+	revalidatePath(`/medlem`)
+	revalidatePath(`/medlem/${body.slug}`)
+	return new Response(console.log(body))
+}
