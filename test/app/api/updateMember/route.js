11959c9bac6043993262ca528db4b60ada3ee83e
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-08 10:35:31 +0100

    added revalidate tag insted of path

--- test/app/api/updateMember/route.js
+++ test/app/api/updateMember/route.js
@@ -1,10 +1,11 @@
+import { revalidateTag } from 'next/cache'
+
 export async function POST(req) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
 	const body = await req.json()
 
 	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, certifications: body.certifications, slug: body.slug })
-	revalidatePath(`/medlem`)
-	revalidatePath(`/medlem/${body.slug}`)
+	revalidateTag('companyData')
 	return new Response(console.log(body))
 }
