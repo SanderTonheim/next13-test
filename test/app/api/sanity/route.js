1e58fb476f41c576a3625d23a875136db72957bf
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-08 14:49:23 +0100

    removed await

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -10,7 +10,7 @@ export async function POST(req) {
 		const record = index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, certifications: body.certifications, slug: body.slug }).wait()
 		return record
 	}
-	await add(body)
+	add(body)
 	revalidatePath('/medlem')
 	return new Response(console.log({ body: body, record: add() }))
 }
