06703ecb63637de6c360bade8a5038b1f822b4e4
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-08 15:12:31 +0100

    added update member func

--- test/app/api/updateMember/route.js
+++ test/app/api/updateMember/route.js
@@ -1,12 +1,17 @@
+import { revalidatePath } from 'next/cache'
 import algoliasearch from 'algoliasearch'
-import { revalidatePath, revalidateTag } from 'next/cache'
 
 export async function POST(req) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
 	const body = await req.json()
 
-	index.partialUpdateObject({ objectID: body._id, name: body.name, tags: body.tag, certifications: body.certifications, slug: body.slug })
-	await revalidatePath(`/medlem/${body.slug}`)
-	return new Response(console.log(body.slug))
+	const updateMember = (body) => {
+		const record = index.partialUpdateObject({ objectID: body._id, name: body.name, tags: body.tag, certifications: body.certifications, slug: body.slug }).wait()
+		return record
+	}
+	await updateMember(body)
+	revalidatePath(`/medlem/${body.slug}`)
+
+	return new Response(console.log(body))
 }
