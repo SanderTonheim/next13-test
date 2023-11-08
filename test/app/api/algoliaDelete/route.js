bb176854fc9ba7b8daa440c57b36d515a1cf232a
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-08 11:51:04 +0100

    added delete object

--- test/app/api/algoliaDelete/route.js
+++ test/app/api/algoliaDelete/route.js
@@ -1,21 +1,12 @@
 import algoliasearch from 'algoliasearch'
 import { revalidatePath } from 'next/cache'
 
-export async function POST(request) {
+export async function POST(req) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
-	try {
-		const { slug } = await request.json()
-		index.deleteObject(slug).then(() => {
-			// done
-		})
+	const { slug } = await req.json()
 
-		revalidatePath(`/medlem`)
-
-		return new Response(console.log(`path is revaldated. ${slug} was removed`))
-	} catch (err) {
-		// If there was an error, Next.js will continue
-		// to show the last successfully generated page
-		return new Response(console.log(err))
-	}
+	index.deleteObject(slug)
+	revalidatePath('/medlem')
+	return new Response(console.log('path deleted', slug))
 }
