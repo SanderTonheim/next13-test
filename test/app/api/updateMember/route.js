32089f9d1e3e81366f65ad496364221495bec5ba
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-08 10:25:48 +0100

    added new route

--- test/app/api/updateMember/route.js
+++ test/app/api/updateMember/route.js
@@ -1,6 +1,3 @@
-import { revalidatePath } from 'next/cache'
-import algoliasearch from 'algoliasearch'
-
 export async function POST(req) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
@@ -8,6 +5,6 @@ export async function POST(req) {
 
 	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, certifications: body.certifications, slug: body.slug })
 	revalidatePath(`/medlem`)
-	// revalidatePath(`/medlem/${body.slug}`)
+	revalidatePath(`/medlem/${body.slug}`)
 	return new Response(console.log(body))
 }
