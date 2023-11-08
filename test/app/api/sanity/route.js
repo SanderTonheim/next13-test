14efc39ea743a408310313cec38479b9ca78407a
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-08 13:50:51 +0100

    removed use server added func

--- test/app/api/sanity/route.js
+++ test/app/api/sanity/route.js
@@ -1,5 +1,3 @@
-'use server'
-
 import { revalidatePath, revalidateTag } from 'next/cache'
 import algoliasearch from 'algoliasearch'
 
@@ -8,12 +6,9 @@ export async function POST(req) {
 	const index = client.initIndex('Members')
 	const body = await req.json()
 
-	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, certifications: body.certifications, slug: body.slug })
-		.wait()
-		.then(() => {
-			revalidatePath('/medlem')
-			revalidatePath(`/medlem/${body.slug}`)
-			console.log('updated')
-		})
-	return new Response(console.log(body))
+	const add = (body) => {
+		const record = index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, certifications: body.certifications, slug: body.slug }).wait()
+		return record
+	}
+	return new Response(console.log({ body: body, record: add }))
 }
