34692d398de44f396ce5766cd66d313b7ff72aec
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 10:55:20 +0100

    log deconstruct body

--- test/app/api/updateMember/route.js
+++ test/app/api/updateMember/route.js
@@ -5,9 +5,11 @@ export async function POST(request) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
 	const body = await request.json()
+	const { test } = await request.json()
 	const slug = body.slug
 	const path = request.nextUrl.origin
 	console.log('path:', path)
+	console.log('test:', test)
 
 	const updateMember = (body) => {
 		const record = index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
