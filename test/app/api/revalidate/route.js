f5524d73825ab3cc75c7bedf3134fe5a6b5c11d1
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 12:32:19 +0100

    adde index and client

--- test/app/api/revalidate/route.js
+++ test/app/api/revalidate/route.js
@@ -3,11 +3,14 @@ import { revalidatePath } from 'next/cache'
 
 export async function GET(request) {
 	const path = request.nextUrl.searchParams.get('path')
+	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
+	const index = client.initIndex('Members')
 
 	if (path) {
 		revalidatePath(`/medlem/${slug}`)
 		return Response.json({ revalidated: true, now: Date.now() })
 	}
+
 	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
 	const body = await request.json()
 	revalidatePath(`/medlem/${body.slug}`)
