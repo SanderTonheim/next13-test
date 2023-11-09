f343b334cc9a3084164fdea8e8e54ed856d17f45
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 10:59:10 +0100

    /

--- test/app/api/updateMember/route.js
+++ test/app/api/updateMember/route.js
@@ -5,6 +5,7 @@ export async function POST(request) {
 	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
 	const index = client.initIndex('Members')
 	const { body } = await request.json()
+	console.log(body)
 	const slug = body.slug
 	const path = request.nextUrl.origin
 	console.log('path:', path)
