dfcae09b31a9096e61ea92ac8b9213e88d91a491
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 12:08:43 +0100

    /

--- test/app/api/revalidate/route.js
+++ test/app/api/revalidate/route.js
@@ -19,17 +19,15 @@
 // }
 
 export async function GET(request) {
-	const path = request.nextUrl.searchParams.get('path')
-	console.log(path)
-
-	if (path) {
-		revalidatePath(path)
+	console.log(request)
+	if (1 > 0) {
+		index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
 		return Response.json({ revalidated: true, now: Date.now() })
+	} else {
+		return Response.json({
+			revalidated: false,
+			now: Date.now(),
+			message: 'Missing path to revalidate',
+		})
 	}
-
-	return Response.json({
-		revalidated: false,
-		now: Date.now(),
-		message: 'Missing path to revalidate',
-	})
 }
