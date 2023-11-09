5066cb2af86ca89e7c2d016dd12a935b25523221
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 12:46:57 +0100

    /

--- test/app/api/revalidate/route.js
+++ test/app/api/revalidate/route.js
@@ -2,6 +2,9 @@ import algoliasearch from 'algoliasearch'
 import { revalidatePath } from 'next/cache'
 
 export async function POST(request) {
+	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
+	const index = client.initIndex('Members')
+
 	const body = await request.json()
 
 	const updateMember = (body) => {
