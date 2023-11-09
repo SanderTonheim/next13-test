96da09ec51bd7ef434c5b74ed2965bf5d86ddf17
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 12:16:09 +0100

    /

--- test/app/api/revalidate/route.js
+++ test/app/api/revalidate/route.js
@@ -1,21 +1,14 @@
 import algoliasearch from 'algoliasearch'
 export async function GET(request) {
-	if (request) {
-		const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
-		const index = client.initIndex('Members')
+	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
+	const index = client.initIndex('Members')
 
-		const body = await request.json()
-		console.log(request)
+	const body = await request.json()
+	console.log(request)
+	console.log(body)
 
-		// index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
-		return Response.json({ revalidated: true, now: Date.now() })
-	} else {
-		return Response.json({
-			revalidated: false,
-			now: Date.now(),
-			message: 'Missing path to revalidate',
-		})
-	}
+	// index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
+	return Response.json({ revalidated: true, now: Date.now() })
 }
 
 // export async function POST(request) {
