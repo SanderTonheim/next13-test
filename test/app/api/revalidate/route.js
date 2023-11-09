05e7936b90cd2b908c91533146b4dead05128793
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 12:39:08 +0100

    /

--- test/app/api/revalidate/route.js
+++ test/app/api/revalidate/route.js
@@ -5,16 +5,15 @@ export async function GET(request) {
 	const path = 1
 
 	if (path > 0) {
-		return Response.json({ revalidated: true, now: Date.now() })
+		const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
+		const index = client.initIndex('Members')
+		const body = await request.json()
+		index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
+		revalidatePath(`/medlem/${body.slug}`)
+		return Response.json({ revalidated: true, data: body })
 	}
 
-	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
-	const index = client.initIndex('Members')
-	const body = await request.json()
-	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
-	revalidatePath(`/medlem/${body.slug}`)
-
-	return Response.json({ message: `${body}` })
+	return Response.json({ message: 'not validated' })
 }
 
 // export async function POST(request) {
