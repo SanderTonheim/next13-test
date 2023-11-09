9e9faaa4a722b9cae5edb6a28dfebccc7773d4c0
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 12:13:54 +0100

    /

--- test/app/api/revalidate/route.js
+++ test/app/api/revalidate/route.js
@@ -1,13 +1,13 @@
 import algoliasearch from 'algoliasearch'
 export async function GET(request) {
-	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
-	const index = client.initIndex('Members')
+	if (request) {
+		const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
+		const index = client.initIndex('Members')
 
-	const body = await request.json()
-	console.log(request)
+		const body = await request.json()
+		console.log(request)
 
-	if (1 > 0) {
-		index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
+		// index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
 		return Response.json({ revalidated: true, now: Date.now() })
 	} else {
 		return Response.json({
