d3be25918045cef406984be687e8f3bd74b63043
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 12:29:33 +0100

    /

--- test/app/api/revalidate/route.js
+++ test/app/api/revalidate/route.js
@@ -1,18 +1,21 @@
-// import algoliasearch from 'algoliasearch'
+import algoliasearch from 'algoliasearch'
 import { revalidatePath } from 'next/cache'
 
 export async function GET(request) {
 	const path = request.nextUrl.searchParams.get('path')
 
 	if (path) {
-		revalidatePath(path)
+		revalidatePath(`/medlem/${slug}`)
 		return Response.json({ revalidated: true, now: Date.now() })
 	}
+	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
+	const body = await request.json()
+	revalidatePath(`/medlem/${body.slug}`)
 
 	return Response.json({
 		revalidated: false,
 		now: Date.now(),
-		message: path,
+		message: body,
 	})
 }
 
