dbeb236b25f04626a14183b5ee51835d09fa36d4
Sander Tonheim <sandert.kodehode@gmail.com>
2023-11-09 12:33:46 +0100

    /

--- test/app/api/revalidate/route.js
+++ test/app/api/revalidate/route.js
@@ -3,16 +3,16 @@ import { revalidatePath } from 'next/cache'
 
 export async function GET(request) {
 	const path = request.nextUrl.searchParams.get('path')
-	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
-	const index = client.initIndex('Members')
 
 	if (path) {
 		revalidatePath(`/medlem/${slug}`)
 		return Response.json({ revalidated: true, now: Date.now() })
 	}
 
-	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
+	const client = algoliasearch('CL6X1N5OU8', 'b5cf6abddecca4efc7e0b6234e818950')
+	const index = client.initIndex('Members')
 	const body = await request.json()
+	index.saveObject({ objectID: body._id, name: body.name, tags: body.tag, connections: body.connections, certifications: body.certifications, slug: body.slug }).wait()
 	revalidatePath(`/medlem/${body.slug}`)
 
 	return Response.json({
